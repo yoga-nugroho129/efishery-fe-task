f304739f8565556985fc7250019bb080e973a332
yoga isnaini nugroho <yoga.nugroho129@gmail.com>
2021-06-10 00:52:57 +0700

    style:create footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <div className="container">
+        &copy; Copyright <br />
+        Developed by <a href="https://www.linkedin.com/in/yoga-nugroho129/" target="_blank" rel="noopener noreferrer">Yoga Isnaini Nugroho</a> for eFishery
+      </div>
+    </footer>
   )
 }
 

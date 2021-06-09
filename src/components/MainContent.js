e6c0654532a6dda6bef848f50a607ed5f0a8e5a6
yoga isnaini nugroho <yoga.nugroho129@gmail.com>
2021-06-10 00:37:35 +0700

    feat:formatting price & date on data list

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,158 +2,130 @@ import React, { useState, useMemo } from 'react'
 import TableData from './Table/TableData'
 import { useTable,  useGlobalFilter, useSortBy, usePagination  } from 'react-table'
 import { TablePagination, TableSearchField } from '.'
+import { FaEdit, FaTrashAlt } from 'react-icons/fa'
+import moment from 'moment'
 
 const MainContent = () => {
   const [dataList, setDataList] = useState([
     {
-      "uuid": "kotlin.random.XorWowRandom@64a9d2f",
-      "komoditas": "Gabus",
-      "area_provinsi": "BALI",
-      "area_kota": "BULELENG",
-      "size": "140",
-      "price": "25000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "kotlin.random.XorWowRandom@64a9d2f",
+      komoditas: "Gabus",
+      area_provinsi: "BALI",
+      area_kota: "BULELENG",
+      size: "140",
+      price: "25000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "kotlin.random.XorWowRandom@ea1ec3b",
-      "komoditas": "Bandeng",
-      "area_provinsi": "SULAWESI BARAT",
-      "area_kota": "MAMUJU UTARA",
-      "size": "140",
-      "price": "20000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "kotlin.random.XorWowRandom@ea1ec3b",
+      komoditas: "Bandeng",
+      area_provinsi: "SULAWESI BARAT",
+      area_kota: "MAMUJU UTARA",
+      size: "140",
+      price: "20000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "e776757d-2547-44d8-84fe-1258b5ec945c",
-      "komoditas": "Sepat",
-      "area_provinsi": "SUMATERA BARAT",
-      "area_kota": "PADANG PARIAMAN",
-      "size": "130",
-      "price": "23000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "e776757d-2547-44d8-84fe-1258b5ec945c",
+      komoditas: "Sepat",
+      area_provinsi: "SUMATERA BARAT",
+      area_kota: "PADANG PARIAMAN",
+      size: "130",
+      price: "23000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "a9e10e7d-d385-49b5-ac55-fe6d04f300af",
-      "komoditas": "Patin Albino",
-      "area_provinsi": "LAMPUNG",
-      "area_kota": "LAMPUNG TIMUR",
-      "size": "120",
-      "price": "20000",
-      "tgl_parsed": "2020-06-08T12:14:31.719Z",
-      "timestamp": "1591593271719"
+      uuid: "a9e10e7d-d385-49b5-ac55-fe6d04f300af",
+      komoditas: "Patin Albino",
+      area_provinsi: "LAMPUNG",
+      area_kota: "LAMPUNG TIMUR",
+      size: "120",
+      price: "20000",
+      tgl_parsed: "2020-06-08T12:14:31.719Z",
+      timestamp: "1591593271719"
     },
     {
-      "uuid": "57d8839f-6583-46c9-8e5d-67175363332d",
-      "komoditas": "Teri",
-      "area_provinsi": "BANTEN",
-      "area_kota": "CIREBON",
-      "size": "50",
-      "price": "90000",
-      "tgl_parsed": "2020-06-09T15:08:58.122Z",
-      "timestamp": "1591690138122"
+      uuid: "57d8839f-6583-46c9-8e5d-67175363332d",
+      komoditas: "Teri",
+      area_provinsi: "BANTEN",
+      area_kota: "CIREBON",
+      size: "50",
+      price: "90000",
+      tgl_parsed: "2020-06-09T15:08:58.122Z",
+      timestamp: "1591690138122"
     },
     {
-      "uuid": "kotlin.random.XorWowRandom@64a9d2f",
-      "komoditas": "Gabus",
-      "area_provinsi": "BALI",
-      "area_kota": "BULELENG",
-      "size": "140",
-      "price": "25000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "kotlin.random.XorWowRandom@64a9d2f",
+      komoditas: "Gabus",
+      area_provinsi: "BALI",
+      area_kota: "BULELENG",
+      size: "140",
+      price: "25000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "kotlin.random.XorWowRandom@ea1ec3b",
-      "komoditas": "Bandeng",
-      "area_provinsi": "SULAWESI BARAT",
-      "area_kota": "MAMUJU UTARA",
-      "size": "140",
-      "price": "20000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "kotlin.random.XorWowRandom@ea1ec3b",
+      komoditas: "Bandeng",
+      area_provinsi: "SULAWESI BARAT",
+      area_kota: "MAMUJU UTARA",
+      size: "140",
+      price: "20000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "e776757d-2547-44d8-84fe-1258b5ec945c",
-      "komoditas": "Sepat",
-      "area_provinsi": "SUMATERA BARAT",
-      "area_kota": "PADANG PARIAMAN",
-      "size": "130",
-      "price": "23000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "e776757d-2547-44d8-84fe-1258b5ec945c",
+      komoditas: "Sepat",
+      area_provinsi: "SUMATERA BARAT",
+      area_kota: "PADANG PARIAMAN",
+      size: "130",
+      price: "23000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "a9e10e7d-d385-49b5-ac55-fe6d04f300af",
-      "komoditas": "Patin Albino",
-      "area_provinsi": "LAMPUNG",
-      "area_kota": "LAMPUNG TIMUR",
-      "size": "120",
-      "price": "20000",
-      "tgl_parsed": "2020-06-08T12:14:31.719Z",
-      "timestamp": "1591593271719"
+      uuid: "a9e10e7d-d385-49b5-ac55-fe6d04f300af",
+      komoditas: "Patin Albino",
+      area_provinsi: "LAMPUNG",
+      area_kota: "LAMPUNG TIMUR",
+      size: "120",
+      price: "20000",
+      tgl_parsed: "2020-06-08T12:14:31.719Z",
+      timestamp: "1591593271719"
     },
     {
-      "uuid": "57d8839f-6583-46c9-8e5d-67175363332d",
-      "komoditas": "Teri",
-      "area_provinsi": "BANTEN",
-      "area_kota": "CIREBON",
-      "size": "50",
-      "price": "90000",
-      "tgl_parsed": "2020-06-09T15:08:58.122Z",
-      "timestamp": "1591690138122"
+      uuid: "57d8839f-6583-46c9-8e5d-67175363332d",
+      komoditas: "Teri",
+      area_provinsi: "BANTEN",
+      area_kota: "CIREBON",
+      size: "50",
+      price: "90000",
+      tgl_parsed: "2020-06-09T15:08:58.122Z",
+      timestamp: "1591690138122"
     },
     {
-      "uuid": "kotlin.random.XorWowRandom@64a9d2f",
-      "komoditas": "Gabus",
-      "area_provinsi": "BALI",
-      "area_kota": "BULELENG",
-      "size": "140",
-      "price": "25000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
+      uuid: "kotlin.random.XorWowRandom@64a9d2f",
+      komoditas: "Gabus",
+      area_provinsi: "BALI",
+      area_kota: "BULELENG",
+      size: "140",
+      price: "25000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
     {
-      "uuid": "kotlin.random.XorWowRandom@ea1ec3b",
-      "komoditas": "Bandeng",
-      "area_provinsi": "SULAWESI BARAT",
-      "area_kota": "MAMUJU UTARA",
-      "size": "140",
-      "price": "20000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
-    },
-    {
-      "uuid": "e776757d-2547-44d8-84fe-1258b5ec945c",
-      "komoditas": "Sepat",
-      "area_provinsi": "SUMATERA BARAT",
-      "area_kota": "PADANG PARIAMAN",
-      "size": "130",
-      "price": "23000",
-      "tgl_parsed": "2020-06-08T10:46:21.374Z",
-      "timestamp": "1591587981374"
-    },
-    {
-      "uuid": "a9e10e7d-d385-49b5-ac55-fe6d04f300af",
-      "komoditas": "Patin Albino",
-      "area_provinsi": "LAMPUNG",
-      "area_kota": "LAMPUNG TIMUR",
-      "size": "120",
-      "price": "20000",
-      "tgl_parsed": "2020-06-08T12:14:31.719Z",
-      "timestamp": "1591593271719"
-    },
-    {
-      "uuid": "57d8839f-6583-46c9-8e5d-67175363332d",
-      "komoditas": "Teri",
-      "area_provinsi": "BANTEN",
-      "area_kota": "CIREBON",
-      "size": "50",
-      "price": "90000",
-      "tgl_parsed": "2020-06-09T15:08:58.122Z",
-      "timestamp": "1591690138122"
+      uuid: "kotlin.random.XorWowRandom@ea1ec3b",
+      komoditas: "Bandeng",
+      area_provinsi: "SULAWESI BARAT",
+      area_kota: "MAMUJU UTARA",
+      size: "140",
+      price: "20000",
+      tgl_parsed: "2020-06-08T10:46:21.374Z",
+      timestamp: "1591587981374"
     },
   ])
 
@@ -178,13 +150,42 @@ const MainContent = () => {
     {
       Header: "Harga",
       accessor: "price",
+      Cell: item => <span>{new Intl.NumberFormat("id-ID", {style: "currency", currency: "IDR"}).format(item.value)}</span>
     },
     {
-      Header: "Per Tanggal",
+      Header: "Tanggal",
       accessor: "tgl_parsed",
+      Cell: item => <span>{moment(item.value).format('D MMM YYYY')}</span>
+    },
+    {
+      Header: "Aksi",
+      accessor: item => (
+        <div className="row-btn">
+          <button 
+            className="edit-btn"
+            title={`Edit "${item.komoditas}" Info`}
+            onClick={() => alert(item.komoditas)}
+          >
+            <FaEdit />
+          </button>
+          <button 
+            className="delete-btn"
+            title={`Hapus "${item.komoditas}"`}
+            onClick={() => alert(item.price)}
+          >
+            <FaTrashAlt size={12} />
+          </button>
+        </div>
+      ),
+      disableSortBy: true
     },
   ], [])
-  const tableInstance = useTable({ columns, data }, useGlobalFilter, useSortBy, usePagination)
+  const tableInstance = useTable(
+    { columns, data },
+    useGlobalFilter,
+    useSortBy,
+    usePagination
+  )
   const {
     getTableProps,
     getTableBodyProps,
